667a95debed512a76f5532046bf13a7c05ed6606
Miguel Lopez <malopezp.91@gmail.com>
2017-04-02 20:53:35 -0700

    soliving issue with ports

--- server.js
+++ server.js
@@ -30,5 +30,10 @@
     });
 
     // listen (start app with node server.js) ======================================
-    app.listen(8080);
-    console.log("App listening on port 8080");
+    var port = process.env.PORT || 8080
+    
+    app.listen(port, function() {
+    console.log("App is running on port " + port);
+    });
+
+    //console.log("App listening on port 8080");
